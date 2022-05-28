1b7a0ebfa7fef058bce1045ac2ce280064244bf0
dmytro.popov <podotris@gmail.com>
2022-05-28 21:09:22 +0300

    dz60 experiment

--- keyboards/dztech/dz60rgb/v2/rules.mk
+++ keyboards/dztech/dz60rgb/v2/rules.mk
@@ -2,7 +2,7 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+BOOTLOADER = halfkay
 
 # Build Options
 #   change yes to no to disable
@@ -16,6 +16,8 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
 AUDIO_ENABLE = no              # Audio output
-RGB_MATRIX_ENABLE = yes        # Use RGB matrix
+RGB_MATRIX_ENABLE = no        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
 NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
+
+### Disabled RGB_MATRIX_ENABLE because it gets slow without actual controller
